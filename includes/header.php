88b69fcfdaf7f67ce762fe042117f743b363dee9
La Ode Muhamad Dirga <dirgad58@gmail.com>
2026-01-06 19:01:38 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -21,8 +21,8 @@ $currentPage = $_SERVER['REQUEST_URI'];
    <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@600;700;800;900&display=swap" rel="stylesheet">
-  <link href="/IMK/assets/css/app.css" rel="stylesheet">
-  <link rel="stylesheet" href="/IMK/assets/css/landing-teal-solid.css">
+  <link href="/IMK-MentalCare-/assets/css/app.css" rel="stylesheet">
+  <link rel="stylesheet" href="/IMK-MentalCare-/assets/css/landing-teal-solid.css">
   <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="assets/css/bootstrap-icons.css" rel="stylesheet">
 <link href="assets/css/mentalcare.css" rel="stylesheet">
